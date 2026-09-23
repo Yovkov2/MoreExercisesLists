ed3935aebf49d9d22c2682e3656c2eec8cec3c20
YovkovIT <143735176+Yovkov2@users.noreply.github.com>
2026-09-23 19:39:41 +0300

    01. Messaging

--- MoreExercisesLists/Program.cs
+++ MoreExercisesLists/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+string text = Console.ReadLine();
+
+string result = "";
+
+foreach(int number in numbers)
+{
+    int sum = 0;
+    int current = number;
+
+    while (current > 0)
+    {
+        sum += current % 10;
+        current /= 10;
+    }
+    int index = sum % text.Length;
+
+    result += text[index];
+
+    text = text.Remove(index, 1);
+}
+Console.WriteLine(result);
